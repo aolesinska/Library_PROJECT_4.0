4fe6c0ddc86cef9fe4985a733354363ae1f5ddb8
aolesinska <aleksandra.olesinska@microsoft.wsei.edu.pl>
2022-07-16 15:08:27 +0200

    Add Dals with functions

---
+++ LIBRARY_PROJECT_4.0/Dals/CategoryDal.cs
@@ -0,0 +1,24 @@
+﻿using LIBRARY_PROJECT_4._0.DalModels.CategoryModels;
+using LIBRARY_PROJECT_4._0.LibraryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARY_PROJECT_4._0.Dals
+{
+    internal class CategoryDal
+    {
+        private LibraryDB3Entities db = new LibraryDB3Entities();
+        public IList<CategoryDalModelForSelector> getCategoryList =>
+            db.Categories.Select
+            (
+                category => new CategoryDalModelForSelector
+                {
+                    ID = category.ID,
+                    Name = category.Categorie,
+                }).ToList();
+
+    }
+}
---
+++ LIBRARY_PROJECT_4.0/Dals/StatusDal.cs
@@ -0,0 +1,23 @@
+﻿using LIBRARY_PROJECT_4._0.DalModels.StatusModel;
+using LIBRARY_PROJECT_4._0.LibraryModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARY_PROJECT_4._0.Dals
+{
+    internal class StatusDal
+    {
+        private LibraryDB3Entities db = new LibraryDB3Entities();
+        public IList<StatusDalModelForSelector> getStatusList =>
+            db.Status.Select
+            (
+                status => new StatusDalModelForSelector
+                {
+                    ID = status.ID,
+                    Name = status.Status1,
+                }).ToList();
+    }
+}
