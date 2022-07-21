a89728fc76b3e0ee2e299bb5990ea33d00a4cb05
aolesinska <aleksandra.olesinska@microsoft.wsei.edu.pl>
2022-07-21 21:29:06 +0200

    Create autor validators

---
+++ LIBRARY_PROJECT_4.0/ValidationRules/AuthorValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARY_PROJECT_4._0.ValidationRules
+{
+    class AuthorValidation : BasicValidation
+    {
+        public ValidationResult FNameValidation(string text)
+        {
+            var result = this.ValidateInput(text, "First Name", 30, 3);
+            if (!result.IsValidate)
+                return result;
+
+            return new ValidationResult { IsValidate = true, ErrorMsg = null };
+        }
+        public ValidationResult LNameValidation(string text)
+        {
+            var result = this.ValidateInput(text, "Last Name", 30, 2);
+            if (!result.IsValidate)
+                return result;
+
+            return new ValidationResult { IsValidate = true, ErrorMsg = null };
+        }
+    }
+
+}
